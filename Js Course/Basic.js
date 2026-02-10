9a1cf85db340b58ecbeeb9ca0eff84cedfd9d571
aminuddin1234 <amiamin987@gmail.com>
2026-02-10 17:54:31 +0800

    JS Lesson

---
+++ Js Course/Basic.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+console.log(2+ 5);
+document.body.innerHTML = "<h1>Hello Aminuddin</h1>";
+console.log(10.9 * 2 + 20.95);
+console.log(10.9 * 2 + 20.95 + 4.99);
+console.log(10.76 * 0.8);
+console.log(0.6 + 0.3);
+console.log(20.95 + 7.99);
+console.log((2095 + 799) / 100);
+console.log(Math.round(20.95 + 7.99));
+console.log(Math.round((2095 + 799) * 0.1) / 100 );
+console.log("some" + "thing");
+console.log("some" + " 3 " + "thing"); // string concatenation
+console.log("$" + (109 * 2 + 2095) / 100); // string concatenation with expression
+console.log("Items: " + (1+1) + ": $" + (109 * 2 + 2095) / 100);
+//alert("Items: " + (1+1) + ": $" + (109 * 2 + 2095) / 100);
+console.log("I'm \n \t Spiderman");
+console.log(`I'm \t Spiderman`);
+console.log(`Items: ${1+1} : $${(109 * 2 + 2095) / 100}`); //interpolation & template literal
+console.log(`some
+    thing`); //template literal
+
+
