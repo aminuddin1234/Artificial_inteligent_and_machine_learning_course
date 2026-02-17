a6a545a3189db9c3e7a801a4dab7f7e75469a80b
aminuddin1234 <amiamin987@gmail.com>
2026-02-17 16:41:26 +0800

    training

--- AI & ML courses/Day 31 - Continue Learning/Control_Flow.py
+++ AI & ML courses/Day 31 - Continue Learning/Control_Flow.py
@@ -28,3 +28,43 @@ print(sequence)
 
 for num in sequence:
   print(num)
+
+#syntax while loop
+#while condition:
+  # code to be executed
+count = 5
+while count > 0:
+  print(count)
+  count -= 1
+print("Done")
+
+for i in range(5):
+  if i == 3:
+    break # exit the loop
+  print(i)
+print("Done")
+
+for i in range(5):
+  if i == 3:
+    continue  # skip the iteration
+  print(i)
+print("Done")
+
+for i in range(10):
+  if i % 3 == 0:
+    continue  # skip the iteration
+  print(i)
+print("Done")
+
+## Exercise:
+num = int(input("Enter a number: 2"))
+
+if num > 1:
+  for i in range(2, int(num**0.5) + 1):
+    if num % i == 0:
+      print(f"{num} is not a prime number")
+      break
+  else:
+    print(f"{num} is a prime number")
+else:
+  print(f"{num} is not a prime number")
